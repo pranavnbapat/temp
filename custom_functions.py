c1940b544e5de5d48f3124bec0b351f856f956ce
Pranav Bapat <pranav.g33k@gmail.com>
2022-04-07 16:17:04 +0200

    Add files via upload

---
+++ custom_functions.py
@@ -0,0 +1,19 @@
+def time_plot(data, x_col, y_col, title):
+    fig, ax = plt.subplots(figsize=(15,5))
+    sns.lineplot(x_col, y_col, data=data, ax=ax, color='mediumblue', label='Total Sales')
+    
+    second = data.groupby(data.date.dt.year)[y_col].mean().reset_index()
+    second.date = pd.to_datetime(second.date, format='%Y')
+    sns.lineplot((second.date + datetime.timedelta(6*365/12)), y_col, data=second, ax=ax, color='red', label='Mean Sales')   
+    
+    ax.set(xlabel = "Date", ylabel = "Sales", title = title)
+    
+    sns.despine()
+
+#Converts all dates of a month to a full month by addin all the sales
+def monthly_sales(data):
+    monthly_data = data.copy()
+    monthly_data.date = monthly_data.date.apply(lambda x: str(x)[:-3])
+    monthly_data = monthly_data.groupby('date')['sales'].sum().reset_index()
+    monthly_data.date = pd.to_datetime(monthly_data.date)
+    return monthly_data
